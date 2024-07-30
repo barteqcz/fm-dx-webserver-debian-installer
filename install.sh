0ea65a0f505b435dfd47d39851695202632e53ec
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-07-30 16:38:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,21 +48,21 @@ make
 make install
 usermod -aG dialout $user
 
-echo "[Unit] \
-Description=xdrd \
-After=network-online.target \
-Wants=network-online.target \
-\
-[Service] \
-ExecStart=/usr/bin/xdrd -s $xdrd_serial_port -p $xdrd_password \
-User=$user \
-Restart=always \
-StandardOutput=syslog \
-StandardError=syslog \
-SyslogIdentifier=xdrd \
-\
-[Install] \
-WantedBy=multi-user.target" | tee /etc/systemd/system/xdrd.service
+echo "[Unit]
+Description=xdrd
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+ExecStart=/usr/bin/xdrd -s $xdrd_serial_port -p $xdrd_password
+User=$user
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=xdrd
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/xdrd.service
 
 chmod 644 /etc/systemd/system/xdrd.service
 systemctl daemon-reload
@@ -76,22 +76,22 @@ npm install
 
 usermod -aG audio $user
 
-echo "[Unit] \
-Description=FM-DX Webserver \
-After=network-online.target xdrd.service \
-Requires=xdrd.service \
-\
-[Service] \
-ExecStart=npm run webserver \
-WorkingDirectory=$build_dir/fm-dx-webserver \
-User=$user \
-Restart=always \
-StandardOutput=syslog \
-StandardError=syslog \
-SyslogIdentifier=fm-dx-webserver \
-\
-[Install] \
-WantedBy=multi-user.target" | tee /etc/systemd/system/fm-dx-webserver.service
+echo "[Unit]
+Description=FM-DX Webserver
+After=network-online.target xdrd.service
+Requires=xdrd.service
+
+[Service]
+ExecStart=npm run webserver
+WorkingDirectory=$build_dir/fm-dx-webserver
+User=$user
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=fm-dx-webserver
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/fm-dx-webserver.service
 
 chmod 644 /etc/systemd/system/fm-dx-webserver.service
 systemctl daemon-reload
